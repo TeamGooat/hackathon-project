bddaaf9b0c41909f4d8673664751bec2d970bf78
Kim Gao <Kimmygao808@gmail.com>
2022-08-27 17:34:37 +1000

    fix: homepage front end styling

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,27 +1,41 @@
 import Header from "../components/Header";
 import Homies from "../images/homies.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 function HomePage() {
   return (
-    <div className='h-screen'>
+    <div className="h-screen">
       <Header unauthenticated />
-      <div className='grid grid-cols-[1fr,1.1fr] gap-2 h-5/6 px-10 overflow-hidden'>
-        <section className='flex flex-col gap-7 rounded-2xl justify-center items-left h-full'>
-          <h1 className='text-5xl font-bold'>Welcome to responsIO</h1>
-          <h1 className='text-4xl font-light'>
-            Get your responses to your questions simple, quick &amp; easy
+      <div className="grid grid-cols-[.9fr,0.7fr] gap-2 h-5/6 px-10 overflow-hidden">
+        <section className="flex flex-col gap-7 rounded-2xl justify-center items-left h-full">
+          <h1 className="text-5xl font-bold font-Almarai">
+            Welcome to responsIO
           </h1>
-          <h1 className='text-3xl font-extralight'>
-            Featuring <b>real-time video collaboration</b> with{" "}
-            <b>colding, math &amp; face-time</b> capabilities
+          <h1 className="text-4xl font-light">
+            Get your responses to your questions quick, fast &amp; easy
           </h1>
-          <div className='flex flex-row gap-7'>
-            <button className='btn'>Join for free</button>
-            <button className='btn'>See how it works</button>
+          <h1 className="text-3xl font-extralight">
+            Featuring <b className="font-bold">real-time video collaboration</b>{" "}
+            with <b className="font-bold">coding, math &amp; face-time</b>{" "}
+            capabilities
+          </h1>
+          <div className="flex flex-row gap-7">
+            <button className="bg-pinky btn text-[#fff] hover:bg-accent">
+              Join for free
+            </button>
+            <button className="btn bg-transparent border-none text-[#fff] underline hover:bg-accent ">
+              {" "}
+              <FontAwesomeIcon
+                icon={faCirclePlay}
+                className="text-3xl pr-3 text-success"
+              />{" "}
+              See how it works
+            </button>
           </div>
         </section>
-        <section className='flex rounded-2xl justify-center items-center'>
-          <img src={Homies} alt='People smiling' />
+        <section className="flex rounded-2xl justify-center items-center ">
+          <img src={Homies} alt="People smiling" />
         </section>
       </div>
     </div>
