603ece5d35374ed09c189bb4ecb6239ab7673f0a
Samantha Tran <stran@Samanthas-MacBook-Air.local>
2022-08-27 15:55:11 +1000

    task for vicky

--- apps/server/src/resources/auth/auth.model.ts
+++ apps/server/src/resources/auth/auth.model.ts
@@ -1,3 +1,7 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 // create a that looks into users table and checks if user with same email or username exists
 // return true if there is , false otherwise
 
